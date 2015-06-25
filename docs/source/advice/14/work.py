989f85679cac4235c2e117f01384abb3a9082a36
zdimon <zdimon77@gmail.com>
2015-06-25 12:24:01 +0300

    work/

--- docs/source/advice/14/work.py
+++ docs/source/advice/14/work.py
@@ -32,3 +32,62 @@ setup(name='django-mapshop',
 python setup.py register
 
 python setup.py sdist upload
+
+# Pager
+
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def listing(request):
+    contact_list = Contacts.objects.all()
+    paginator = Paginator(contact_list, 25) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+
+    return render_to_response('list.html', {"contacts": contacts})
+
+
+
+  {% if  is_paginated %}
+    {% if  page_obj.has_previous %}
+      <a href="?page={{ page_obj.previous_page_number }}">назад</a> |
+    {% endif %}
+    страница {{ page_obj.number }} из {{ page_obj.paginator.num_pages }}
+    {% if  page_obj.has_next %}|
+      <a href="?page={{ page_obj.next_page_number }}">вперед</a>
+    {% endif %}
+  {% endif %}
+
+
+
+
+
+
+
+# gen views
+
+class RecipesView(ListView):
+    queryset = Recipe.objects.all().order_by('-id')
+    template_name = 'recipes_list.html'
+    paginate_by = 10
+    def get_context_data(self, **kwargs):
+        context = super(RecipesView, self).get_context_data(**kwargs)
+        try:
+            from mshop.models import SeoPages
+            seo =  SeoPages.objects.get(alias='recipes')
+            context['seo'] = seo
+        except:
+            pass
+        return context
+
+
+
+
